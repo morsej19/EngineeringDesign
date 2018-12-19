1e4e540fce5ff05f6101cdcaf290f1b59885e1b4
Maximus Reisner <maximusreisner@Maximuss-MacBook-Pro.local>
2018-12-19 11:44:20 -0500

    fixed my dragon code

--- maxr.py
+++ maxr.py
@@ -1,4 +1,5 @@
 def make_dragon():
+	print("")
 	print("       \****__              ____          ")                                  
 	print("         |    *****\_      --/ *\-__       ")                                 
 	print("         /_          (_    ./ ,/----'      ")                                 
@@ -8,4 +9,11 @@ def make_dragon():
 	print("        |    _/  __/ )\ \ _____         *\ ")                                 
 	print("        |\__/   /    ^ ^       \_________| ")
 
-make_dragon()
+number_of_dragons = input("How many dragons? ")
+
+if number_of_dragons > 0:
+	for x in range(0, number_of_dragons):
+		make_dragon()
+	print("")
+else:
+	print("There must be a valid number")
