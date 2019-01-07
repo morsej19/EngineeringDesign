1f865fbc6720c4a7026f79e55284d1a1af751176
morsej19 <46002509+morsej19@users.noreply.github.com>
2019-01-07 15:37:46 -0500

    arms!
    Gerald now has an arm. just one. is it even attached? who knows.

--- brinkbuildthree.py
+++ brinkbuildthree.py
@@ -1,25 +1,45 @@
 import random as rand
-#neckless_gerald = "  o \n \|/ \n / \\"
+#neckless_gerald = "  o \n /|\ \n / \\"
 # o
-#\|/
+#/|\
 #/ \
-def GeraldExtender(neck_size):
 
+bottom=1
+
+def GeraldExtender(neck_size):
 
     print("  o")
     for i in range(neck_size):
         print("  |")
-    print(" \|/")
-    print(" / \\")
 
+def GeraldBottom(bottom):
+    
+    for i in range(bottom):
+        print(" \|/")
+        print(" / \\")
+
+def GeraldArm(arm_size):
+
+    for i in range(arm_size):
+        print("          |")
 
 print("Hello, Gerald has a bizarre disease called Giraffian Neck Syndrome. His neck can grown on command.")
-print("how big should his neck be?")
+print("How big should his neck be?")
 
 user_neck_size = int(input())
 GeraldExtender(user_neck_size)
+GeraldBottom(bottom)
 #print(user_input)
 
+print("How big should arm be?")
+
+arm_size = int(input())
+GeraldExtender(user_neck_size)
+GeraldBottom(bottom)
+GeraldArm(arm_size)
+#print(user_input)
+
+print("Turns out Gerald's arm fell off. Oops.")
 print("How many friends should Gerald have?")
 
 gerald_no_friends = int(input())
